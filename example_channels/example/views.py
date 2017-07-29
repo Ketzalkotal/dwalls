8cbb02d898fbf1d85207311b9f1eda4d571a0f33
James Daley <jdaley@dialsource.com>
2017-07-29 16:38:23 -0700

    makin models

--- example_channels/example/views.py
+++ example_channels/example/views.py
@@ -1,10 +1,15 @@
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
 
+User = get_user_model()
+
 def user_list(request):
-    return render(request, 'example/user_list.html')
+    users = User.objects.select_related('logged_in_user')
+    for user in users:
+        user.status = 'Online' if hasattr(user, 'logged_in_user') else 'Offline'
+    return render(request, 'example/user_list.html', {'users': users})
 
 def log_in(request):
     form = AuthenticationForm()
@@ -21,4 +26,15 @@ def log_in(request):
 def log_out(request):
     logout(request)
     return redirect(reverse('example:log_in'))
+
+def sign_up(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('example:log_in'))
+        else:
+            print(form.errors)
+    return render(request, 'example/sign_up.html', {'form': form})
 
